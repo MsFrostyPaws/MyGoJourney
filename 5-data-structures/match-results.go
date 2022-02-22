ee566dace13e45f8da0d3fad932bb59545e3da52
MsFrostyPaws <100154307+MsFrostyPaws@users.noreply.github.com>
2022-02-22 12:09:16 +0300

    Update match-results.go

--- 5-data-structures/match-results.go
+++ 5-data-structures/match-results.go
@@ -1,3 +1,13 @@
+/*
+	Program to analyze sport match results and calculate the points of the given team.
+	The match results are stored in an array called results.
+	Each match has one of the following results:
+		"w" - won
+		"l" - lost
+		"d" - draw
+
+	A win adds three points, a draw adds one point, and a lost match does not add any points.
+*/
 package main
 
 import "fmt"
